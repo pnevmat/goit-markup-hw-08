f657494e8aea3417a2dda84384604dec7e5f9b7d
Vadim Kravchenko <pnevmat001@gmail.com>
2020-11-11 15:02:54 +0200

    Updates button js code for portfolio.html

--- js/portfolio-buttons.js
+++ js/portfolio-buttons.js
@@ -4,192 +4,55 @@ const appsButtonRef = document.querySelector('button[apps]');
 const designButtonRef = document.querySelector('button[design]');
 const marketingButtonRef = document.querySelector('button[marketing]');
 
-const webSitesCardsRef = document.querySelectorAll('li[web-site]');
-const appsCardsRef = document.querySelectorAll('li[app]');
-const designCardsRef = document.querySelectorAll('li[design]');
-const marketingCardsRef = document.querySelectorAll('li[marketing]');
+const allCardsRef = [...document.querySelectorAll('li[web-site]'),
+    ...document.querySelectorAll('li[app]'),
+    ...document.querySelectorAll('li[design]'),
+    ...document.querySelectorAll('li[marketing]')];
 
-let listLength;
+webSitesButtonRef.addEventListener('click', onWebSitesButtonClick);
+appsButtonRef.addEventListener('click', onAppsButtonClick);
+designButtonRef.addEventListener('click', onDesignButtonClick);
+marketingButtonRef.addEventListener('click', onMarketingButtonClick);
+allButtonRef.addEventListener('click', onAllButtonClick);
 
-webSitesButtonRef.addEventListener('click', () => {
-    listLength = appsCardsRef.length;
-    for (let i = 0; i < listLength; i += 1) {
-        if (i < listLength) {
-            appsCardsRef[i].classList.add('portfolio-items-hidden');
+function onWebSitesButtonClick() {
+    allCardsRef.forEach((element, i) => {
+        if (element.attributes[1].name !== 'web-site') {
+            allCardsRef[i].classList.add('portfolio-items-hidden');
         } else {
-            break;
+            allCardsRef[i].classList.remove('portfolio-items-hidden');
         }
-    }
-    listLength = designCardsRef.length;
-    for (let i = 0; i < listLength; i += 1) {
-        if (i < listLength) {
-            designCardsRef[i].classList.add('portfolio-items-hidden');
+    });
+};
+function onAppsButtonClick() {
+    allCardsRef.forEach((element, i) => {
+        if (element.attributes[1].name !== 'app') {
+            allCardsRef[i].classList.add('portfolio-items-hidden');
         } else {
-            break;
+            allCardsRef[i].classList.remove('portfolio-items-hidden');
         }
-    }
-    listLength = marketingCardsRef.length;
-    for (let i = 0; i < listLength; i += 1) {
-        if (i < listLength) {
-            marketingCardsRef[i].classList.add('portfolio-items-hidden');
+    });
+};
+function onDesignButtonClick() {
+    allCardsRef.forEach((element, i) => {
+        if (element.attributes[1].name !== 'design') {
+            allCardsRef[i].classList.add('portfolio-items-hidden');
         } else {
-            break;
+            allCardsRef[i].classList.remove('portfolio-items-hidden');
         }
-    }
-    listLength = webSitesCardsRef.length;
-    for (let i = 0; i < listLength; i += 1) {
-        webSitesCardsRef[i].classList.remove('portfolio-items-hidden');
-        if (listLength <= 3) {
-            webSitesCardsRef[i].classList.add('chosen-three');
+    });
+};
+function onMarketingButtonClick() {
+    allCardsRef.forEach((element, i) => {
+        if (element.attributes[1].name !== 'marketing') {
+            allCardsRef[i].classList.add('portfolio-items-hidden');
         } else {
-            webSitesCardsRef[i].classList.add('chosen-more-three');
+            allCardsRef[i].classList.remove('portfolio-items-hidden');
         }
-    }
-});
-
-appsButtonRef.addEventListener('click', () => {
-    listLength = webSitesCardsRef.length;
-    for (let i = 0; i < listLength; i += 1) {
-        webSitesCardsRef[i].classList.add('portfolio-items-hidden');
-    }
-    listLength = designCardsRef.length;
-    for (let i = 0; i < listLength; i += 1) {
-        if (i < listLength) {
-            designCardsRef[i].classList.add('portfolio-items-hidden');
-        } else {
-            break;
-        }
-    }
-    listLength = marketingCardsRef.length;
-    for (let i = 0; i < listLength; i += 1) {
-        if (i < listLength) {
-            marketingCardsRef[i].classList.add('portfolio-items-hidden');
-        } else {
-            break;
-        }
-    }
-    listLength = appsCardsRef.length;
-    for (let i = 0; i < listLength; i += 1) {
-        if (i < listLength) {
-            appsCardsRef[i].classList.remove('portfolio-items-hidden');
-            if (listLength <= 3) {
-                appsCardsRef[i].classList.add('chosen-three');
-            } else {
-                appsCardsRef[i].classList.add('chosen-more-three');
-            }
-        } else {
-            break;
-        }
-    }
-});
-
-designButtonRef.addEventListener('click', () => {
-    listLength = webSitesCardsRef.length;
-    for (let i = 0; i < listLength; i += 1) {
-        webSitesCardsRef[i].classList.add('portfolio-items-hidden');
-    }
-    listLength = appsCardsRef.length;
-    for (let i = 0; i < listLength; i += 1) {
-        if (i < listLength) {
-            appsCardsRef[i].classList.add('portfolio-items-hidden');
-        } else {
-            break;
-        }
-    }
-    listLength = marketingCardsRef.length;
-    for (let i = 0; i < listLength; i += 1) {
-        if (i < listLength) {
-            marketingCardsRef[i].classList.add('portfolio-items-hidden');
-        } else {
-            break;
-        }
-    }
-    listLength = designCardsRef.length;
-    for (let i = 0; i < listLength; i += 1) {
-        if (i < listLength) {
-            designCardsRef[i].classList.remove('portfolio-items-hidden');
-            if (listLength <= 3) {
-                designCardsRef[i].classList.add('chosen-three');
-            } else {
-                designCardsRef[i].classList.add('chosen-more-three');
-            }
-        } else {
-            break;
-        }
-    }
-});
-
-marketingButtonRef.addEventListener('click', () => {
-    listLength = webSitesCardsRef.length;
-    for (let i = 0; i < listLength; i += 1) {
-        webSitesCardsRef[i].classList.add('portfolio-items-hidden');
-    }
-    listLength = appsCardsRef.length;
-    for (let i = 0; i < listLength; i += 1) {
-        if (i < listLength) {
-            appsCardsRef[i].classList.add('portfolio-items-hidden');
-        } else {
-            break;
-        }
-    }
-    listLength = designCardsRef.length;
-    for (let i = 0; i < listLength; i += 1) {
-        if (i < listLength) {
-            designCardsRef[i].classList.add('portfolio-items-hidden');
-        } else {
-            break;
-        }
-    }
-    listLength = marketingCardsRef.length;
-    for (let i = 0; i < listLength; i += 1) {
-        if (i < listLength) {
-            marketingCardsRef[i].classList.remove('portfolio-items-hidden');
-            if (listLength <= 3) {
-                marketingCardsRef[i].classList.add('chosen-three');
-            } else {
-                marketingCardsRef[i].classList.add('chosen-more-three');
-            }
-        } else {
-            break;
-        }
-    }
-});
-
-allButtonRef.addEventListener('click', () => {
-    listLength = webSitesCardsRef.length;
-    for (let i = 0; i < listLength; i += 1) {
-        webSitesCardsRef[i].classList.remove('portfolio-items-hidden');
-        webSitesCardsRef[i].classList.remove('chosen-three');
-        webSitesCardsRef[i].classList.remove('chosen-more-three');
-    }
-    listLength = appsCardsRef.length;
-    for (let i = 0; i < listLength; i += 1) {
-        if (i < listLength) {
-            appsCardsRef[i].classList.remove('portfolio-items-hidden');
-            appsCardsRef[i].classList.remove('chosen-three');
-            appsCardsRef[i].classList.remove('chosen-more-three');
-        } else {
-            break;
-        }
-    }
-    listLength = designCardsRef.length;
-    for (let i = 0; i < listLength; i += 1) {
-        if (i < listLength) {
-            designCardsRef[i].classList.remove('portfolio-items-hidden');
-            designCardsRef[i].classList.remove('chosen-three');
-            designCardsRef[i].classList.remove('chosen-more-three');
-        } else {
-            break;
-        }
-    }
-    listLength = marketingCardsRef.length;
-    for (let i = 0; i < listLength; i += 1) {
-        if (i < listLength) {
-            marketingCardsRef[i].classList.remove('portfolio-items-hidden');
-            marketingCardsRef[i].classList.remove('chosen-three');
-            marketingCardsRef[i].classList.remove('chosen-more-three');
-        } else {
-            break;
-        }
-    }
-});
+    });
+};
+function onAllButtonClick() {
+    allCardsRef.forEach((element, i) => {
+        allCardsRef[i].classList.remove('portfolio-items-hidden');
+    });
+};
